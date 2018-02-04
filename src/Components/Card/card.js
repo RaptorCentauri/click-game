89cb06e4a23da38cac25e1a34e8503aa6fd9e11e
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-02-04 14:35:27 -0600

    working on persistent click state

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./card.css";
 
+// class Card extends React.Component{
+//
+//
+//   render(props){
+//     return(
+//       <div className="card" onClick={() => props.handleClick(props.id)}>
+//         <img src={this.props.image} alt={this.props.name}/>
+//       </div>
+//     );
+//   }
+// }
+
+
+
 const Card = props =>
-  <div className="card">
-    <img src={props.image} alt={props.name}/>
-  </div>
+      <div className="card" onClick={() => props.handleClick(props.id)}>
+        <img src={props.image} alt={props.name}/>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 export default Card;
--- src/Components/GameBoard/gameBoard.js
+++ src/Components/GameBoard/gameBoard.js
@@ -3,12 +3,48 @@ import "./gameBoard.css";
 import Card from "../Card/card.js"
 import images from "../../images.json"
 
-const GameBoard = () =>
-  <div className="game-board">
-    {images.map(i => <Card name={i.image} image={`/assets/Images/${i.image}`}/>)}
-    {/* <Card image={`/assets/Images/Bane.jpg`}/> */}
 
-  </div>
+const shuffle = (a) => {
+  for (let i = a.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+}
 
+shuffle(images);
+
+
+class GameBoard extends React.Component{
+
+  state = {
+      clicked: "false"
+    };
+
+  handleClick = id => {
+    console.log(this.key);
+    console.log(this.state.clicked);
+    this.setState({clicked: "true"});
+    this.setState();
+  }
+
+
+  render(){
+    return(
+      <div className="game-board">
+        {images.map(i => <Card key={i.id} name={i.image} image={`/assets/Images/${i.image}`} clicked={this.state.clicked} handleClick={this.handleClick}/>)}
+      </div>
+    )
+  }
+}
 
 export default GameBoard;
+
+
+
+
+
+// state = {
+//     clicked: "false"
+//   };
+
+// handleClick = () => this.setState({clicked: "true"});
